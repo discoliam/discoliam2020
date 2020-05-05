2ad08185bc8afe987771f3f7adf8ffae22c618d9
Liam Richardson <hello@discoliam.com>
2020-05-05 11:52:37 -0700

    Added scrollbar support cos why not

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,11 +6,13 @@ const postcss = require('gulp-postcss');
 const postcssPresetEnv = require('postcss-preset-env');
 const atImport = require('postcss-import');
 const postcssNesting = require('postcss-nesting');
+const postcssScrollbar = require('postcss-scrollbar');
 const purgecss = require('@fullhuman/postcss-purgecss');
 const cssnano = require('cssnano');
 const autoprefixer = require('autoprefixer');
 const webpack = require('webpack-stream');
 const named = require('vinyl-named');
+
 
 //==============================================================================
 // Variables
@@ -24,9 +26,10 @@ gulp.task('css', function() {
     atImport(),
     postcssNesting(),
     postcssPresetEnv({ stage: 0 }),
+    postcssScrollbar(),
     purgecss({
       content: ['src/**/*.liquid', 'src/**/*.md'],
-      whitelist: ["a","abbr","address","area","b","blockquote","br","button","canvas","caption","cite","code","dd","del","dl","dt","em","fieldset","figure","h1","h2","h3","h4","h5","h6","hgroup","hr","i","img","input","label","li","link","mark","ol","p","pre","q","s","small","span","strong","sub","sup","table","tbody","thead","tfoot","td","th","tr","time","ul","video","heading-link"]
+      whitelist: ["body", "a","abbr","address","area","b","blockquote","br","button","canvas","caption","cite","code","dd","del","dl","dt","em","fieldset","figure","h1","h2","h3","h4","h5","h6","hgroup","hr","i","img","input","label","li","link","mark","ol","p","pre","q","s","small","span","strong","sub","sup","table","tbody","thead","tfoot","td","th","tr","time","ul","video","heading-link"]
     }),
     autoprefixer(),
     cssnano({
